793a1ba7a8b96e775ca77abbc189ffc264110e6f
ZIENxc <76558675+ZIENxc@users.noreply.github.com>
2025-09-18 23:47:14 +0800

    Update test_mimic_cxr.sh

--- test_mimic_cxr.sh
+++ test_mimic_cxr.sh
@@ -1,14 +1,14 @@
 CUDA_VISIBLE_DEVICES=1, python main_test.py \
 --n_gpu 1 \
 --image_dir ../dataset/mimic_cxr/images/ \
---ann_path data/mimic_cxr/mimic_annotation_promptmrg.json \
+--ann_path data/mimic_cxr/mimic_annotation.json \
 --dataset_name mimic_cxr \
 --gen_max_len 150 \
 --gen_min_len 100 \
 --batch_size 16 \
---save_dir results/promptmrg \
+--save_dir results/ \
 --seed 456789 \
 --clip_k 21 \
 --beam_size 3 \
---load_pretrained results/promptmrg/model_best_t_mid_gk3.23.pth
-# --load_pretrained results/promptmrg/model_best.pth
+--load_pretrained results/model_best_t_mid_gk3.23.pth
+# --load_pretrained results/model_best.pth
